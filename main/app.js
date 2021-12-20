4e51acad0a1174e82fe8a91e6c8ff11ee3c92041
前端小小白 <84488812+hsjzhcq@users.noreply.github.com>
2021-12-20 16:09:11 +0800

    Update app.js

--- main/app.js
+++ main/app.js
@@ -3,8 +3,6 @@
  * @Date: 2021-12-16 18:29:22
  * @LastEditTime: 2021-12-20 16:02:17
  * @LastEditors: Please set LastEditors
- * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- * @FilePath: \我的博客\js\app.js
  */
 (() => {
     var typeHome = ["https://zjy2.icve.com.cn", "https://mooc.icve.com.cn"], //平台类型数组
